50e2fe37f6bcbc056dc5580432cf80e7b841651c
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2025-05-18 20:00:14 -0300

    instalado projeto

---
+++ src/main/java/com/jwt2/security_jwt2/SecurityJwt2Application.java
@@ -0,0 +1,13 @@
+package com.jwt2.security_jwt2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityJwt2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityJwt2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jwt2/security_jwt2/SecurityJwt2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwt2.security_jwt2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityJwt2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
