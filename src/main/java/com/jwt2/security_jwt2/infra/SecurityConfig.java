7d8141611dfd459ef0f8fb28fdcf45e548d023c4
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2025-05-19 10:21:02 -0300

    Concluuido processo autenticaçao jtw com auth0 - iniciar autenticação provider externo

--- src/main/java/com/jwt2/security_jwt2/infra/SecurityConfig.java
+++ src/main/java/com/jwt2/security_jwt2/infra/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.jwt2.security_jwt2.infra;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,10 +17,13 @@ public class SecurityConfig {
         return http
             .authorizeHttpRequests(
                 authorizeConfig ->{
-                    authorizeConfig.anyRequest().permitAll();
-                    //authorizeConfig.anyRequest().authenticated();
+                    //authorizeConfig.anyRequest().permitAll();
+                    authorizeConfig.requestMatchers("/public").permitAll();
+                    authorizeConfig.requestMatchers("/logout").permitAll();
+                    authorizeConfig.anyRequest().authenticated();// para  permitir acesso substituir por permitAll
                 }
             )
+            .formLogin(Customizer.withDefaults())
             .build();
         
         
